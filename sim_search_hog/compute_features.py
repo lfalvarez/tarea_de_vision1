3c25f05500dc0b1f6020dec91f3f439133e9e823
Felipe Álvarez <luis.alvarez@thoughtworks.com>
2019-11-27 20:20:15 -0300

    Agregando el mAP

--- sim_search_hog/compute_features.py
+++ sim_search_hog/compute_features.py
@@ -11,11 +11,25 @@ import os
 import argparse
 import numpy as np
 
+
+class Config():
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.image_size = (128, 128)
+        self.grid_size = (8, 8)
+        self.orientations = 8
+        str_file = os.path.join(str_datadir, "{}.list".format(file_name))
+        the_files_ = data.read_data(str_file)
+        self.filenames, self.labels = zip(*the_files_)
+        self.features = np.zeros((len(self.filenames), dim), np.float32)
+        return features
+
+
 def calculate_distance_to_all_images(str_datadir, original_hogs, original_labels):
     str_file = os.path.join(str_datadir, "test.list")
     test_files_ = data.read_data(str_file)
     filenames, labels = zip(*test_files_)
-    np_labels = np.array(labels, np.int)
+
     image_size = (128, 128)
     grid_size = (8, 8)
     orientations = 8
@@ -24,6 +38,7 @@ def calculate_distance_to_all_images(str_datadir, original_hogs, original_labels
     n_images = len(filenames)
     features = np.zeros((n_images, dim), np.float32)
     print('feat shape {}'.format(features.shape))
+    sum_of_aps = 0
     for i, (filename, current_label) in enumerate(zip(filenames, labels)) :
         filename = os.path.join(str_datadir,'png_w256', filename)
         image = pai.imread(filename, as_gray = True)
@@ -36,9 +51,32 @@ def calculate_distance_to_all_images(str_datadir, original_hogs, original_labels
 
         sorted_distances = sorted(distances, key=lambda d: d['dist'])
         distances = sorted_distances[:25]
-        relevant_images = list(filter(lambda d: d['is_relevant'], distances))
+        relevant_counter = 0
+        AP = 0
+        for index, d in enumerate(distances):
+            dividendo = 0
+            if d['is_relevant']:
+                relevant_counter += 1
+                dividendo = relevant_counter
+            AP += dividendo / (index + 1)
+        if relevant_counter:
+            AP = AP/relevant_counter
+            sum_of_aps += AP
+
+        '''if relevant_counter:
+            rep = '('
+            counter = 0
+            for i, d in enumerate(distances):
+                if d['is_relevant']:
+                    counter += 1
+                    rep += '{}/{} + '.format(counter, i+1)
+            rep += '0)/{}'.format(relevant_counter)
+            print(rep)
+            print(a_p)'''
         ## frente a la consulta aquí imprimo la cantidad de imagenes relevantes por cada test!
-        print(len(relevant_images))
+        #print(a_p)
+    mAP = (sum_of_aps/ len(filenames))
+    print(mAP)
 
 
 if __name__ == '__main__':
