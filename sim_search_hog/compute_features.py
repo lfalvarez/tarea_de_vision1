3e765304e221b5be21074a6c81f317bd014a206b
Felipe Álvarez <luis.alvarez@thoughtworks.com>
2019-11-27 23:09:50 -0300

    imprimiendo las imagenes

--- sim_search_hog/compute_features.py
+++ sim_search_hog/compute_features.py
@@ -10,22 +10,11 @@ import data
 import os
 import argparse
 import numpy as np
+from PIL import Image
 
 
-class Config():
-    def __init__(self, data_dir):
-        self.data_dir = data_dir
-        self.image_size = (128, 128)
-        self.grid_size = (8, 8)
-        self.orientations = 8
-        str_file = os.path.join(str_datadir, "{}.list".format(file_name))
-        the_files_ = data.read_data(str_file)
-        self.filenames, self.labels = zip(*the_files_)
-        self.features = np.zeros((len(self.filenames), dim), np.float32)
-        return features
 
-
-def calculate_distance_to_all_images(str_datadir, original_hogs, original_labels):
+def calculate_distance_to_all_images(str_datadir, original_hogs, original_labels, original_filenames):
     str_file = os.path.join(str_datadir, "test.list")
     test_files_ = data.read_data(str_file)
     filenames, labels = zip(*test_files_)
@@ -44,13 +33,15 @@ def calculate_distance_to_all_images(str_datadir, original_hogs, original_labels
         image = pai.imread(filename, as_gray = True)
         this_hog = hog.get_hog(image)
         distances = []
-        for index, (other_hog, other_label) in enumerate(zip(original_hogs, original_labels)):
+        for index, (other_hog, other_label, other_filename) in enumerate(zip(original_hogs, original_labels, original_filenames)):
             dist = np.linalg.norm(this_hog - other_hog)
             is_relevant = current_label == other_label ## Es relevante SSI el label del test es igual a la de la imagen cercana.
-            distances.append({'is_relevant': is_relevant, 'dist': dist})
+            datos_de_comparacion = {'is_relevant': is_relevant, 'dist': dist}
+            datos_de_comparacion['other_filename'] = other_filename
+            distances.append(datos_de_comparacion)
 
         sorted_distances = sorted(distances, key=lambda d: d['dist'])
-        distances = sorted_distances[:25]
+        distances = sorted_distances[:10]
         relevant_counter = 0
         AP = 0
         for index, d in enumerate(distances):
@@ -63,18 +54,20 @@ def calculate_distance_to_all_images(str_datadir, original_hogs, original_labels
             AP = AP/relevant_counter
             sum_of_aps += AP
 
-        '''if relevant_counter:
-            rep = '('
-            counter = 0
-            for i, d in enumerate(distances):
-                if d['is_relevant']:
-                    counter += 1
-                    rep += '{}/{} + '.format(counter, i+1)
-            rep += '0)/{}'.format(relevant_counter)
-            print(rep)
-            print(a_p)'''
-        ## frente a la consulta aquí imprimo la cantidad de imagenes relevantes por cada test!
-        #print(a_p)
+        if relevant_counter > 2 and relevant_counter < 5:
+            para_mostrar = [filename]
+            for d in distances[:10]:
+                para_mostrar.append(os.path.join(str_datadir,'png_w256', d['other_filename']))
+            final_dim = (256 * len(para_mostrar), 256)
+            new_im = Image.new('L', final_dim)
+            for j, im_file in enumerate(para_mostrar):
+                im = Image.open(im_file)
+                new_im.paste(im, (j*256, 0))
+            new_im.save('{}_comparado.jpg'.format(i))
+            print('{}_comparado.jpg'.format(i))
+
+
+
     mAP = (sum_of_aps/ len(filenames))
     print(mAP)
 
@@ -111,4 +104,4 @@ if __name__ == '__main__':
     
     np_labels.tofile(str_labels)
     print('labels saved at {}'.format(str_labels))
-    calculate_distance_to_all_images(str_datadir, features, labels)
+    calculate_distance_to_all_images(str_datadir, features, labels, filenames)
